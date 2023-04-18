6c9d9dfdbac02b49d71be855a3db0c72e2c171d1
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-04-18 12:49:55 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+//!  Разметка
+//      <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>
