fb9afd43532e4a7c22cdc363b664cbd300d4f043
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-04-21 18:21:20 +0300

    update
    add fetch and create markup

--- src/api.js
+++ src/api.js
@@ -3,7 +3,10 @@ export default function fetchPictures(query) {
   const API_KEY = '35615782-928d74ab541d750ac5cbbfeab';
   const fields = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-  return fetch(`${URL}?key=${API_KEY}&q=${query}&${fields}&per_page=3`)
-    .then(response => response.json)
-    .then(data => console.log(data));
+  return fetch(`${URL}?key=${API_KEY}&q=${query}&${fields}`).then(response => {
+    if (!response.ok) {
+      throw new Eroor(`${response.status}`);
+    }
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -5,23 +5,62 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 import fetchPictures from './api';
 
-fetchPictures('cat');
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
 
-//!  Разметка
-//      <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>
+searchForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const searchQuery = formElements.searchQuery.value;
+
+  fetchPictures(searchQuery)
+    .then(createCard)
+    .catch(err => {
+      if (err.message === '404') {
+        Notiflix.Notify.failure('Oops, there is no image with that name');
+      }
+    });
+}
+
+function createCard({ hits }) {
+  if (hits.length === 0) {
+    Notiflix.Notify.failure('Not found image');
+  }
+
+  const createCard = hits.reduce(
+    (markup, card) => markup + createMarkup(card),
+    ''
+  );
+
+  gallery.innerHTML = createCard;
+}
+
+function createMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" width="100%"/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>: ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>: ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>: ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>: ${downloads}
+      </p>
+    </div>
+  </div>`;
+}
