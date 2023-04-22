58f6f633889398da93dc84d70929a7830e6cde48
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-04-22 15:27:55 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -89,16 +89,16 @@ function createMarkup({
     <img src="${webformatURL}" alt="${tags}" loading="lazy" width="100%"/>
     <div class="info">
       <p class="info-item">
-        <b>Likes</b>: ${likes}
+        <b>Likes</b>${likes}
       </p>
       <p class="info-item">
-        <b>Views</b>: ${views}
+        <b>Views</b>${views}
       </p>
       <p class="info-item">
-        <b>Comments</b>: ${comments}
+        <b>Comments</b>${comments}
       </p>
       <p class="info-item">
-        <b>Downloads</b>: ${downloads}
+        <b>Downloads</b>${downloads}
       </p>
     </div>
   </div>`;
